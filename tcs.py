c2e4fc58a0858e515ffdf5b43575bef7550d6983
vishnu0211 <111915031+vishnu0211@users.noreply.github.com>
2026-03-24 11:49:39 +0530

    Update tcs.py

--- tcs.py
+++ tcs.py
@@ -1,3 +1,7 @@
+#Soldier Placement (Ranking) ​Problem: The King wants to rank N soldiers using ranks 1 to r. ​Rule 1: No two adjacent soldiers can have the same rank. ​Rule 2: The first soldier's rank is always 1. ​Rule 3: The last soldier's rank must be L ​Goal: Find the number of valid sequences.
+
+#Example N =4 r= 4 L= 4 Then output will be count of ways to place this 1 _ _ 4 O/p was 7
+
 n = int(input())
 r = int(input())
 start = "1"
